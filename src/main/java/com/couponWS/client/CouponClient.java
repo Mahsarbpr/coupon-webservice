ad6a379a7d1f532f83bcca56af8ee88e4ba52094
Mahsa Arabpour <arabpourm13@students.ecu.edu>
2015-11-18 14:19:20 -0500

    coupon web service with junit tests

---
+++ src/main/java/com/couponWS/client/CouponClient.java
@@ -0,0 +1,46 @@
+package com.couponWS.client;
+
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+import Coupon.Coupon;
+//import couponModel.GenericType;
+import javax.ws.rs.core.GenericType;
+
+public class CouponClient {
+private Client client;
+public CouponClient(){
+	client = ClientBuilder.newClient();
+}
+public Coupon ReadCoupon(String SID){
+	int IID=Integer.parseInt(SID);
+	Coupon cc=client.target("http://localhost:8080/coupon-webservice/webapi/myresource/getcoupons").queryParam("var1",IID).request().get(Coupon.class);
+	return cc;
+}
+public Coupon ReadCoupon2(String SID){
+	Coupon cc=client.target("http://localhost:8080/coupon-webservice/webapi/myresource/").path("readcoupon/"+SID).request().get(Coupon.class);
+	return cc;
+}
+public Coupon GetTime(String SID){
+	int IID=Integer.parseInt(SID);
+	Coupon cc=client.target("http://localhost:8080/coupon-webservice/webapi/myresource/gettime").queryParam("var",IID).request().get(Coupon.class);
+	return cc;
+}
+public List<Coupon> FindCouponByTimeC(String SID){
+	//int IID=Integer.parseInt(SID);
+	List<Coupon> cc=client.target("http://localhost:8080/coupon-webservice/webapi/myresource/FindCouponByTime").queryParam("var1",SID).request().get(new GenericType<List<Coupon>>(){});
+	return cc;
+}
+public List<Coupon> FindCouponForItemC(String SID){
+	//int IID=Integer.parseInt(SID);
+	List<Coupon> cc=client.target("http://localhost:8080/coupon-webservice/webapi/myresource/FindCouponForItem").queryParam("var",SID).request().get(new GenericType<List<Coupon>>(){});
+	return cc;
+}
+public List<Coupon> FindCouponByTypeC(String SID){
+	//int IID=Integer.parseInt(SID);
+	List<Coupon> cc=client.target("http://localhost:8080/coupon-webservice/webapi/myresource/FindCouponsByType").queryParam("var2",SID).request().get(new GenericType<List<Coupon>>(){});
+	return cc;
+}
+}
---
+++ src/test/java/com/couponWS/client/CouponClientTest.java
@@ -0,0 +1,61 @@
+package com.couponWS.client;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import Coupon.Coupon;
+
+public class CouponClientTest {
+
+	@Test
+	public void testReadCoupon() {
+		CouponClient client=new CouponClient();
+		Coupon c=client.ReadCoupon("13");
+		System.out.println(c);
+		assertNotNull(c);
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testReadCoupon2() {
+		CouponClient client=new CouponClient();
+		Coupon c=client.ReadCoupon2("13");
+		System.out.println(c);
+		assertNotNull(c);
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testGetTime(){
+		CouponClient client=new CouponClient();
+		Coupon c=client.GetTime("13");
+		System.out.println(c);
+		assertNotNull(c);
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testFindCouponByTime(){
+		CouponClient client=new CouponClient();
+		List<Coupon> c=client.FindCouponByTimeC("2015-11-18");
+		System.out.println(c);
+		assertNotNull(c);
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testFindCouponForItem(){
+		CouponClient client=new CouponClient();
+		List<Coupon> c=client.FindCouponForItemC("item5");
+		System.out.println(c);
+		assertNotNull(c);
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testFindCouponByType(){
+		CouponClient client=new CouponClient();
+		List<Coupon> c=client.FindCouponByTypeC("1");
+		System.out.println(c);
+		assertNotNull(c);
+		//fail("Not yet implemented");
+	}
+}
